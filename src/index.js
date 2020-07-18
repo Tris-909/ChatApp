f80072eb96d35238dc5022693a4fadcdab718875
= <tranminhtri9090@gmail.com>
2020-07-18 18:08:42 +1000

    Create-react-App, download all the relevant packages, setUp basic Route

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import registerServiceWorker from './registerServiceWorker';
+
+//** COMPONENTS */
+import App from './components/App';
+import Login from './components/Auth/Login';
+import SignUp from './components/Auth/SignUp';
+
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+const Root = () => (
+    <Router>
+        <Switch>
+            <Route exact path="/" component={App} />   
+            <Route  path="/login" component={Login} />   
+            <Route  path="/signup" component={SignUp} />   
+        </Switch>
+    </Router>
+);
+
+ReactDOM.render(<Root />, document.getElementById('root'));
+registerServiceWorker();
