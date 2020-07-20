d37a489ded121c915062e336030cd1a336b1758e
= <tranminhtri9090@gmail.com>
2020-07-20 11:32:06 +1000

    Adding ColorPanel, Messages, MessagesForm and MessagesHeader Component

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import {Segment, Comment} from 'semantic-ui-react';
+import MessagesHeader from './MessagesHeader';
+import MessageForm from './MessageForm';
 
 class Messages extends React.Component {
     render() {
         return(
-            <div>Messages</div>
+            <React.Fragment>
+                <MessagesHeader />
+                <Segment>
+                    <Comment.Group style={{maxWidth: '100%'}} className="messages">
+                    {/* Message */}
+                    </Comment.Group>
+                </Segment>
+                <MessageForm />
+            </React.Fragment>
         );
     }
 }
